4f16e59bf7854fabd2e393745dd995d305122c9f
Dev <Dev@Michaels-MBP-2020.local>
2021-11-17 20:01:59 -0800

    exercise and practice completed

--- itp_week_1/day_3/exercise.py
+++ itp_week_1/day_3/exercise.py
@@ -5,16 +5,19 @@
 lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # 1. loop through the lowercase and print each element
-
+for letter in lowercase:
+    print(letter)
 # 2. loop through the lowercase and print the capitalization of each element
-
+for letter in lowercase:
+    print(letter.upper())
 # MEDIUM
 
 # 1. create a new variable called uppercase with an empty list
-
+uppercase = []
 # 2. loop through the lowercase list
     # 2a. append the capitalization of each element to the uppercase list
-
+for letter in lowercase:
+    uppercase.append(letter.upper())
 # HARD
 
 # A safe password has a minimum of (1) uppercase, (1) lowercase, (1) number, (1) special character.
@@ -28,34 +31,56 @@ special_char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
     # has_lowercase
     # has_number
     # has_special_char
-
+has_uppercase = False
+has_lowercase = False
+has_number = False
+has_special_char = False
 # 2. loop through the string password (same as a list)
 # OR you can create a new list variable of the string password
 # using list(string) NOTE: assign it a new variable as such:
 # password_list = list(password) prior to looping.
 
-# 3. For each iteration of the loop, create a if statement
+# 3. For each iteration of the loop, create an if statement
 # check to see if it exists in any of the list by using IN
 # if it does exist, update the appropriate variable and CONTINUE
 # not break.
 
 # NOTE: to see if it has a number, use range from 0 - 10!
+for value in password:
+    if value in lowercase:
+        has_lowercase = True
+    if value in uppercase:
+        has_uppercase = True
+    if value in special_char:
+        has_special_char = True
+    if value.isdigit():
+        has_number = True
 
 # 4. do a final check to see if all of your variables are TRUE
 # by using the AND operator for all 4 conditions. (This is done for you, uncomment below)
 
-# final_result = has_uppercase == True and has_lowercase == True and has_number == True and has_special_char == True
+final_result = has_uppercase == True and has_lowercase == True and has_number == True and has_special_char == True
 
 # NOTE: we can shorthand this by just checking if the variable exists (returns True)
-#final_result_shorthand = has_uppercase and has_lowercase and has_number and has_special_char
+final_result_shorthand = has_uppercase and has_lowercase and has_number and has_special_char
 # this will fail the same if any one of them is False
 
 # If the final_result is true, print "SAFE STRONG PASSWORD"
 # else, print "Update password: too weak"
 # NOTE: this must be done outside of the loop
-
+if final_result:
+    print("SAFE STRONG PASSWORD")
+else:
+    print("Update password: too weak")
 # BONUS: update the password variable to take in an user input!
-
+    password = input("Enter password: ")
 # NIGHTMARE: in the final check, use another if statement to list why it isn't a strong password!
-
+    if not has_lowercase:
+        print("Missing a lowercase letter")
+    if not has_uppercase:
+        print("Missing an uppercase letter")
+    if not has_special_char:
+        print("Missing special character")
+    if not has_number:
+        print("Missing a number")
 
--- itp_week_1/day_3/practice.py
+++ itp_week_1/day_3/practice.py
@@ -55,18 +55,25 @@ us_state = ["Alabama",
 ]
 
 # 1. Write the code below that verifies the amount (length) of US States!
-
+if len(us_state) == 50:
+    print("us_states length correct")
+else:
+    print("us_states length incorrect")
 # 2. Create a variable called 'my_state_index' and assign it to the index of your state.
-
+my_state_index = 4
 # 3. let's see if you were right.. uncomment below and run. Do you see your state?
 
-# print("My state is: " + us_state[my_state_index])
+print("My state is: " + us_state[my_state_index])
 
 # 4.Using your state index, let's emphasize some importance to it by *upper*casing it.
+us_state[my_state_index] = us_state[my_state_index].upper()
 
 # 5. POOF. You've been promoted to President! Let's add a new state. I like my list to be alphabetical (which it is)
 # So let's go ahead and create a state that starts with Z and append it to the end of the list.
-
-# 6. There is no state that begins with B! Lets create one and insert it appropriately. (There are 3 A states.)
-
+us_state.append("Zonk")
+# 6. There is no state that begins with B! Lets create one and insert it appropriately. (There are 4 A states.)
+us_state.insert(4, "Breakfast")
 # 7. Does anyone live in Iowa? Do you know anyone that lives there? Is it even real?! Remove it.. Do it president.
+us_state.remove("Iowa")
+
+print(us_state)
