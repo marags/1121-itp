a06fa130fe86dccd6786e1a6fa8b72c2881bed20
Dev <Dev@Michaels-MBP-2020.local>
2021-11-17 18:03:07 -0800

    warmup and practice updates just to be complete

--- itp_week_1/day_2/practice_2.py
+++ itp_week_1/day_2/practice_2.py
@@ -11,3 +11,10 @@
 # Take an user's input
 # Assign a new variable for each step
 # at the end, use an if statement to see if the final is always 5!
+
+user_entry = int(input("Enter a number from 1 - 9 ")) #no input validation
+mult_by_2 = user_entry * 2
+add_10 = mult_by_2 + 10
+divide_in_half = add_10 // 2 #integer division
+final_number = divide_in_half - user_entry
+print(final_number)
--- itp_week_1/day_2/warmup.py
+++ itp_week_1/day_2/warmup.py
@@ -1,5 +1,6 @@
 # ITP Week 1 Day 2 Warm Up
 
 # 1. prompt an user input to ask "What is your age" and assign it to a variable called "user_age"
-
+user_age = input("What is your age? ")
 # 2. print a string concatenation stating "You look amazing for " + user_age
+print("You look amazing for " + user_age)
