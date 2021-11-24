30ee0b6ba71d85d1cc29c49c1636a062ae4ac480
Dev <Dev@Michaels-MBP-2020.local>
2021-11-23 17:05:19 -0800

    exercise completed

--- itp_week_2/day_1/exercise.py
+++ itp_week_2/day_1/exercise.py
@@ -14,14 +14,17 @@ inventory = {
 
 for item in inventory:
     # decrement item by using an assignment operator (Day 2 Lecture line #130)
-
+    inventory[item] -= 1
     # NOTE: recall that item represents the key of the key:value pair
 
 # SCENARIO: REMOVE ANY 0 ITEMS
 
-for item in inventory:
+# for item in inventory:
+for key, value in dict(inventory).items():
     # use an if statement to check if the value is 0 and then remove it
-
+    if value == 0:
+        del inventory[key]
+print(inventory)
 #If your group has completed the above exercise, move on to tuples, sets and nested dictionaries
 
 dogs = {
@@ -44,4 +47,14 @@ dogs = {
 }
 
 # Loop through the dogs dictionary and create a SET of dog names.
+dog_names = set()
+for dog in dogs:
+    for name in dogs[dog]:
+        dog_names.add(name)
+
+print(dog_names)
+
 # Loop through the dogs and create a TUPLE combining each dog's name with its breed.
+for dog in dogs:
+    for name in dogs[dog]:
+        print((name, dogs[dog][name]["breed"]))
