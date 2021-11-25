2754ad79aff7dcf68a81bc1730bebc743249d806
Dev <Dev@Michaels-MBP-2020.local>
2021-11-24 17:17:27 -0800

    practice completed

--- itp_week_2/day_3/practice.py
+++ itp_week_2/day_3/practice.py
@@ -5,4 +5,18 @@ new_releases = ["Encino Man", "I know what you did last Summer", "Phantom Menace
 
 #Create a function defined as "movie_listing" which takes one parameter, called "movies".  Write a function that will loop through a list and print out each item.  Then call that function, passing it the my_movies list as an argument.
 
+def movie_listing(movies):
+    for movie in movies:
+        print(movie)
+
+# movie_listing(my_movies)
+
 #Define a function called "update_movies" that will loop through a list parameter, and use the insert() method on each item to the my_movies list.  Call that function using the new_releases list as the argument.  Then call the movie_listing function passing the "new_releases" as the argument.  It should print the updated list.
+
+def update_movies(list):
+    for item in list:
+        # my_movies.insert(0,item)
+        my_movies.append(item)
+
+update_movies(new_releases)
+movie_listing(my_movies)
