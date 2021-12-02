bfd95b4549034d2e984fd9ff2e61f2b040f9ae33
Dev <Dev@Michaels-MBP-2020.local>
2021-12-01 19:43:16 -0800

    sandbox updated call for sheet names (get_sheet_names() is deprecated)

--- itp_week_3/day_3/sandbox.py
+++ itp_week_3/day_3/sandbox.py
@@ -8,16 +8,17 @@ filename = "/Users/Dev/Code/Git_Repositories/VetsInTech/Python Fundamentals/1121
 my_workbook = openpyxl.load_workbook(filename)
 
 # get list of sheet names
-sheet_list = my_workbook.get_sheet_names()
+#sheet_list = my_workbook.get_sheet_names() # <----- deprecated
+sheet_list = my_workbook.sheetnames # updated method
 
 print(sheet_list)
 
-my_sheet = my_workbook.get_sheet_by_name('Sheet1')
+# my_sheet = my_workbook.get_sheet_by_name('Sheet1')
 
-my_sheet["A8"] = "YOU'VE"
-my_sheet["B8"] = "BEEN"
-my_sheet["C8"] = "HACKED"
+# my_sheet["A8"] = "YOU'VE"
+# my_sheet["B8"] = "BEEN"
+# my_sheet["C8"] = "HACKED"
 
-filename_copy = "/Users/Dev/Code/Git_Repositories/VetsInTech/Python Fundamentals/1121-itp/itp_week_3/day_3/lecture_test_save.xlsx"
+# filename_copy = "/Users/Dev/Code/Git_Repositories/VetsInTech/Python Fundamentals/1121-itp/itp_week_3/day_3/lecture_test_save.xlsx"
 
-my_workbook.save(filename_copy)
+# my_workbook.save(filename_copy)
